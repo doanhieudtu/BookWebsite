cadb4ee831e78b27f1971823d1024a30c97bfc14
DELL <doanhieudtu@gmail.com>
2018-06-01 22:50:27 +0700

    commit ApiAjax

--- src/com/bansachonline/springmvc/interfacedao/GopYDao.java
+++ src/com/bansachonline/springmvc/interfacedao/GopYDao.java
@@ -1,7 +1,9 @@
 package com.bansachonline.springmvc.interfacedao;
 
+import com.bansachonline.springmvc.model.GopY;
+
 /**
  * Created by DELL on 05/25/2018.
  */
-public interface GopYDao {
+public interface GopYDao extends GenericDao<Integer,GopY> {
 }
