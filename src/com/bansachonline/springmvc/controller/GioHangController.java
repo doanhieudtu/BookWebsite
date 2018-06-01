3eef34bb1a51ed2a571705d065cc7331a98e9d3b
DELL <doanhieudtu@gmail.com>
2018-06-01 22:49:24 +0700

    commit ApiAjax

--- src/com/bansachonline/springmvc/controller/GioHangController.java
+++ src/com/bansachonline/springmvc/controller/GioHangController.java
@@ -2,6 +2,9 @@ package com.bansachonline.springmvc.controller;
 
 import javax.servlet.http.HttpSession;
 
+import com.bansachonline.springmvc.model.Sach;
+import com.bansachonline.springmvc.service.ChuDeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +12,21 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.bansachonline.springmvc.model.GioHang;
 
+import java.util.List;
+
 @SessionAttributes({"User","GioHang"})
 @Controller
 @RequestMapping("/GioHang")
 public class GioHangController {
+
+	@Autowired
+	ChuDeService chuDeService;
 	@RequestMapping("/")
 	public String GioHang(HttpSession ss, ModelMap mm) {
 		GioHang gioHang= new GioHang();
+		try {
+			mm.put("lsChuDe",chuDeService.ShowAll() );
+		}catch (Exception e){}
 		try {
 			gioHang= (GioHang) ss.getAttribute("GioHang");
 		} catch (Exception e) {
